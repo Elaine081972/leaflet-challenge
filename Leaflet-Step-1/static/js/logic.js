51f8ee095056076989c9f1da9126b9885a035173
Elaine Hamilton <elainehamilton@elaines-mbp.lan>
2020-10-21 13:20:38 -0700

    update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -44,22 +44,22 @@ function createFeatures(earthquakeData) {
       });
     }
   });
-  let legend = L.control({position: "bottomright"});
-  legend.onAdd = function (map) {
-    let div = L.DomUtil.create("div", "info legend"),
-        grades = [-8, 10, 30, 50, 70, 90],
-        labels = [];
+  // let legend = L.control({position: "bottomright"});
+  // legend.onAdd = function (map) {
+  //   let div = L.DomUtil.create("div", "info legend"),
+  //       grades = [-8, 10, 30, 50, 70, 90],
+  //       labels = [];
 
-    for (var i = 0; i < grades.length; i++) {
-      div.innerHTML +=
-        '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i +1] + '<br>' : '+');
-    }
-    return div;
+  //   for (var i = 0; i < grades.length; i++) {
+  //     div.innerHTML +=
+  //       '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+  //       grades[i] + (grades[i + 1] ? '&ndash;' + grades[i +1] + '<br>' : '+');
+  //   }
+  //   return div;
 
     
-  };
-  legend.addTo(map);
+  // };
+  // legend.addTo(map);
 
   //sending our earthquakes layer to the createMap function
   createMap(earthquakes, mags);
